5f07f9cedf9a89f6f1515e366d58c6605d8e46b1
MailFlow <developer@thestackone.com>
2026-05-19 05:04:52 +0000

    Deploy MailFlow to GitHub Pages

---
+++ _next/static/eTZlGkjKLOPQviMalHBKf/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mailflow//_next/:path+",
+        "destination": "/mailflow/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
